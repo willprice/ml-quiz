f38a3f7a72b21023ce2bf8e0b656597b90a1ba26
Will Price <will.price94@gmail.com>
2016-11-14 18:23:54 +0000

    [travis] Generate tarball

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,21 +5,28 @@ GIT="${GIT:-git}"
 
 QUIZ="${1:-ml.quiz}"
 QUIZ_HTML="${QUIZ%%.quiz}.html"
-QUIZ_TEMP="$(mktemp)"
+QUIZ_TARBALL="64496_15444.tar.gz"
 
 "$GIT" checkout master
+"$GIT" remote add deploy "git@github.com:willprice/ml-quiz.git"
 
 [[ ! -f "$QUIZ" ]] && {
     echo "$QUIZ not found";
     exit 1;
 }
+echo yes | "$PYTHON" ./resources/compile_quiz.py --tarball "$QUIZ"
+[[ -f "$QUIZ_TARBALL" ]] || {
+    echo "Could not find generated tarball";
+    exit 1;
+}
 
 "$PYTHON" ./resources/compile_quiz.py "$QUIZ"
-[[ ! -f "$QUIZ_HTML" ]] && exit 1;
-mv "${QUIZ_HTML}" "${QUIZ_TEMP}"
+[[ -f "$QUIZ_HTML" ]] || {
+    echo "Could not find generated html";
+    exit 1;
+}
+mv "$QUIZ_HTML" index.html
 
-mv "${QUIZ_TEMP}" index.html
-"$GIT" remote add deploy "git@github.com:willprice/ml-quiz.git"
 "$GIT" add .
 "$GIT" commit -m "Generate quiz ($(date))"
 "$GIT" push --force --quiet deploy master:gh-pages
