ec70a0209616845a5099f272d55205984b447a58
Will Price <will.price94@gmail.com>
2016-11-14 14:04:23 +0000

    Initial travis build setup

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,15 +3,28 @@ PYTHON="${PYTHON:-python2}"
 GIT="${GIT:-git}"
 
 QUIZ="${1:-ml.quiz}"
-QUIZ_TEMP="$(mktemp)"
 QUIZ_HTML="${QUIZ%%.quiz}.html"
+QUIZ_TEMP="$(mktemp)"
 
 "$GIT" checkout master
+
+[[ ! -f "$QUIZ" ]] && {
+    echo "$QUIZ not found";
+    exit 1;
+}
+
 "$PYTHON" ./resources/compile_quiz.py "$QUIZ"
+[[ ! -f "$QUIZ_HTML" ]] && exit 1;
 mv "${QUIZ_HTML}" "${QUIZ_TEMP}"
-"$GIT" checkout gh-pages
+
+"$GIT" fetch --all
+"$GIT" checkout --orphan gh-pages
 mv "${QUIZ_TEMP}" index.html
+"$GIT" status index.html | grep 'modified' 2>&1 > /dev/null
+if [[ $? -ne 0 ]]; then
+    echo "No changes to push, exiting"
+    exit 0;
+fi
 "$GIT" add index.html
 "$GIT" commit -m "Generate quiz ($(date))"
 "$GIT" push -u origin gh-pages
-"$GIT" checkout -
---
+++ .travis/git-setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git config --global user.name "Travis CI"
+git config --global user.email "will.price94+travis@gmail.com"
