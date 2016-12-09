015f90820f5ecff96c48302308182c81f8f40492
Will Price <will.price94@gmail.com>
2016-12-09 21:57:18 +0000

    Split ml.yaml into a file per question

--- yaml2json.py
+++ yaml2json.py
@@ -2,5 +2,18 @@
 import sys
 import yaml
 import json
+import os
+
+
+def yaml_include(loader, node):
+    yaml_top_level_dir = os.path.dirname(loader.name)
+    include_relative_path = node.value
+    include_path = os.path.join(yaml_top_level_dir, include_relative_path)
+
+    with file(include_path) as f:
+        return yaml.load(f)
+
+
+yaml.add_constructor("!include", yaml_include)
 
 json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)
